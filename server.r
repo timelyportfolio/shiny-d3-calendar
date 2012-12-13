8258e4936ac11f771ab1d1b5f3556838b3a356d3
timelyportfolio <kent.russell@timelyportfolio.com>
2012-12-13 15:47:19 -0600

    added some comments to help code readers

--- server.r
+++ server.r
@@ -6,7 +6,9 @@ shinyServer(function(input, output) {
   #use the structure from trestletechnology example to load data
   data <- reactive(function(){
     getSymbols(paste("^",input$indexTicker,sep=""))
+    #combine date from xts and coredata to get a data.frame in the format best for JSON pass in Shiny
     data <- cbind(format(index(get(input$indexTicker)),"%Y-%m-%d"),coredata(get(input$indexTicker)))
+    #name columns same as the example
     colnames(data) <- c("Date","Open","High","Low","Close","Volume","Adj Close")
     data
   })
